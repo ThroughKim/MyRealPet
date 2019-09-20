f02dbaf894383dc2a2a07d46f5dcb84c79ae4115
ThroughKim <sulewoo@naver.com>
2019-09-20 23:26:37 +0900

    Add: Add module

---
+++ MyRealPet/Modules/Add/AddPetViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  AddPetViewModel.swift
+//  MyRealPet
+//
+//  Created by KimSeulWoo on 20/09/2019.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class AddPetViewModel: NSObject {
+
+}
